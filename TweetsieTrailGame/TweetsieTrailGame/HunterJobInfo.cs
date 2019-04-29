2d2a8c4c27a365a072c2b33be35dda515dbf5ae5
Ryan Kelly <rpkelly@my.milligan.edu>
2019-04-29 11:05:12 -0400

    Started Hunter(Player) class. Added Todo

---
+++ TweetsieTrailGame/TweetsieTrailGame/HunterJobInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TweetsieTrailGame
+{
+    class HunterJobInfo
+    {
+        private string name;
+        private int health;
+        private int strength;
+        private int scoreMultiplier;
+        private int startingMoney;
+
+        public HunterJobInfo(string name, int health, int strength, int scoreMultiplier, int startingMoney)
+        {
+            this.name = name;
+            this.health = health;
+            this.strength = strength;
+            this.scoreMultiplier = scoreMultiplier;
+            this.startingMoney = startingMoney;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int Health
+        {
+
+        }
+    }
+}
