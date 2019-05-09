d9f6eb134a3b6befcb08588c3562124c354cebea
Ryan Kelly <rpkelly@my.milligan.edu>
2019-05-08 22:15:44 -0400

    started work on TweetsieFileMana

---
+++ TweetsieTrailGame/TweetsieTrailGame/FileManager/ObjectList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TweetsieTrailGame
+{
+    class ObjectList
+    {
+        public List<string> HeaderRow;
+        public List<List<string>> ObjectRows;
+
+        public ObjectList()
+        {
+            HeaderRow = new List<string>();
+            ObjectRows = new List<List<string>>();
+        }
+
+    }
+}
